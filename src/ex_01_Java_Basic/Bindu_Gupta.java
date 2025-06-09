b748737ee5ebc560b2dc02d24f2c053f83d61c3c
bindut <bindu.venkat07@gmail.com>
2025-06-09 18:32:17 +0530

    9th June 2025

---
+++ src/ex_01_Java_Basic/Bindu_Gupta.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Bindu_Gupta {
+    public static void main(String[] Bindu) {
+        System.out.println("Hi");
+
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab007_Comments_Shortcuts.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab007_Comments_Shortcuts {
+//    This code will print the Hi
+//    Jr QA - this will print in the console
+
+
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
