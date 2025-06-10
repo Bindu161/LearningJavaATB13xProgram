8aff213a5fc8dbb2d846fb3a3d5b891205673740
bindut <bindu.venkat07@gmail.com>
2025-06-10 13:53:10 +0530

    10th June 2025

---
+++ src/ex_02_Java_Basic/Lab018.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basic;
+
+public class Lab018 {
+    public static void main(String[] args) {
+//        int _ =65;
+        int $ =65;
+        int yes =65;
+        int this_is_a_very_long_name_in_the_class_hello_$_akakaka =89;
+    }
+}
