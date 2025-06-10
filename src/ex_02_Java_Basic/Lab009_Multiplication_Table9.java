3edf1058e543b47c4985c4a28b008aed566543b5
bindut <bindu.venkat07@gmail.com>
2025-06-10 13:53:10 +0530

    10th June 2025

---
+++ src/ex_02_Java_Basic/Lab009_Multiplication_Table9.java
@@ -0,0 +1,48 @@
+package ex_02_Java_Basic;
+
+import java.sql.SQLOutput;
+
+public class Lab009_Multiplication_Table9 {
+    public static void main(String[] args) {
+        int a= 9;
+        int b= 1;
+        System.out.printf("%d * %d=%d",a,b, a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d * %d=%d", a,b, a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d *%d=%d", a,b, a*b);
+        System.out.println();
+        b++;
+        System.out.printf("%d * %d=%d",a,b, a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d * %d=%d", a,b, a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d *%d=%d", a,b, a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d * %d=%d",a,b, a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d * %d=%d", a,b, a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d * %d=%d", a,b, a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d * %d=%d", a,b,a*b);
+        System.out.println();
+        b++;
+    }
+}
---
+++ src/ex_03_Literals/Lab027_Final_Example.java
@@ -0,0 +1,4 @@
+package ex_03_Literals;
+
+public class Lab027_Final_Example {
+}
