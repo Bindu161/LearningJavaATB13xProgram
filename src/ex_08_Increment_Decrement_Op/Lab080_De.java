1d191921e5a853b9dfb87b0de1e09995477b0f0a
bindut <bindu.venkat07@gmail.com>
2025-06-13 23:47:42 +0530

    June 13th 2025

---
+++ src/ex_08_Increment_Decrement_Op/Lab080_De.java
@@ -0,0 +1,10 @@
+package ex_08_Increment_Decrement_Op;
+
+public class Lab080_De {
+    public static void main(String[] args) {
+        int a = 10;
+        int result_post = a--;
+//        System.out.println(result_post);
+        System.out.println(a);
+    }
+}
---
+++ src/ex_08_Increment_Decrement_Op/Lab081_De2.java
@@ -0,0 +1,10 @@
+package ex_08_Increment_Decrement_Op;
+
+public class Lab081_De2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int result = --a;
+        System.out.println(result);
+        System.out.println(a);
+    }
+}
