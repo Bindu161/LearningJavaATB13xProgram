aaa52b628318f52a9f9cbe88b5f31904a9761201
bindut <bindu.venkat07@gmail.com>
2025-06-13 23:02:41 +0530

    June 13th 2025

---
+++ src/ex_08_Increment_Decrement_Op/Lab072_In_De_Op.java
@@ -0,0 +1,8 @@
+package ex_08_Increment_Decrement_Op;
+
+public class Lab072_In_De_Op {
+    public static void main(String[] args) {
+        String age_string = args [0];
+        System.out.println(age_string);
+    }
+}
