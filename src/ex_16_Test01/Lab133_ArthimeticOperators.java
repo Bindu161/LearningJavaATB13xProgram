7207ea91c4a9c63b747d0aff2880216c664fa93b
bindut <bindu.venkat07@gmail.com>
2025-06-22 19:29:33 +0530

    June 22nd 2025

---
+++ src/ex_16_Test01/Lab133_ArthimeticOperators.java
@@ -0,0 +1,23 @@
+package ex_16_Test01;
+
+public class Lab133_ArthimeticOperators {
+    public static void main(String[] args) {
+
+            int n1 = 10;
+            int n2 = 5;
+
+            int add = n1 + n2;
+            int sub = n1 - n2;
+            int multiplication = n1 * n2;
+            int div = n1 / n2;
+            int mod = n1 % n2;
+
+
+            System.out.println("Addition: " + add);
+            System.out.println("Subtraction: " + sub);
+            System.out.println("Multiplication: " + multiplication);
+            System.out.println("Division: " + div);
+            System.out.println("Modulus: " + mod);
+        }
+    }
+
