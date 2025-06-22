5364625e025f3ca3ae4d2660bc62718d14c85edc
bindut <bindu.venkat07@gmail.com>
2025-06-22 19:22:19 +0530

    June 22nd 2025

---
+++ src/ex_16_Test01/Lab132_VariableDeclarationInitialization.java
@@ -0,0 +1,17 @@
+package ex_16_Test01;
+
+public class Lab132_VariableDeclarationInitialization {
+    public static void main(String[] args) {
+
+                int myInteger = 25;
+                double myDouble = 3.14;
+                boolean isAvailable = true;
+                char myChar = 'A';
+
+                System.out.println("Integer: " + myInteger);
+                System.out.println("Double: " + myDouble);
+                System.out.println("Boolean: " + isAvailable);
+                System.out.println("Character: " + myChar);
+            }
+        }
+
